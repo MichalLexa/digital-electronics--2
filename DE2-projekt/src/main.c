20f1b15cd4bf52753b127839a35ad363c761b71c
MichalLexa <240650@vutbr.cz>
2023-11-29 12:45:58 +0100

    progres

--- DE2-projekt/src/main.c
+++ DE2-projekt/src/main.c
@@ -1,10 +1,10 @@
 
 #ifndef F_CPU
 # define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
-#define HUM_SEN PC0
 #endif
 
-
+#define HUM_SEN PC0
+#define LED_GREEN PB5
 
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
@@ -12,14 +12,19 @@
 #include <UART.h>           // Peter Fleury's UART library
 #include <stdlib.h>         // C library. Needed for number conversions
 #include <gpio.h>
+#include <oled.h>
 // Vzduch 920 - max
 //voda 730 - min
+ uint16_t hum;
 int main(void)
 {
+  GPIO_mode_output(&DDRB, LED_GREEN);
+
   uart_init(UART_BAUD_SELECT(9600, F_CPU));
-  GPIO_mode_input_pullup(&PORTC, PC0);
   
-     ADMUX = ADMUX | (1<<REFS0);
+  
+  // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    ADMUX = ADMUX | (1<<REFS0);
     // Select input channel ADC0 (voltage divider pin)
     ADMUX = ADMUX & ~(1<<MUX3 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0);
     // Enable ADC module
@@ -31,7 +36,10 @@ int main(void)
 
   TIM1_OVF_1SEC;
   TIM1_OVF_ENABLE;
-
+  
+  oled_init(OLED_DISP_ON);
+  
+  
 
   sei();
   
@@ -51,10 +59,36 @@ ISR(TIMER1_OVF_vect)
 }
 ISR(ADC_vect)
 {
-   uint16_t hum;
+
   char string[8];
-  hum =ADC;
+  hum = ADC;
+  hum = (920-hum)*100/270;
+
   itoa(hum, string, 10);  
   uart_puts(string);
   uart_puts("\n");
+   oled_clrscr();
+  oled_gotoxy(0,2);
+  oled_puts("Humidity = ");
+  //oled_gotoxy(1,2);
+  oled_puts(string);
+  oled_puts("%");
+  oled_gotoxy(0,5);
+  oled_puts("Watering: ");
+  if (hum<40)
+  {
+    oled_puts("ON");
+    
+  }
+   else{
+     oled_puts("OFF"); 
+    }
+
+  oled_display();
+    if (hum < 40) 
+  {
+    PORTB = PORTB ^ (1<<LED_GREEN);
+  }
+
+
 }
